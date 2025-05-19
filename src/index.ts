c93aad2c1c9d8a275a556c613e39bffd296d44eb
Benedicte Raae <queen@raae.codes>
2025-05-19 13:00:24 +0200

    fix: query params removed from url
    Query params are removed from the url. This is a fix to make sure they are not. 
    
    Closes #1

--- src/index.ts
+++ src/index.ts
@@ -128,7 +128,7 @@ export default {
 
     // If the URL does not match any patterns, fetch and return the original content
     console.log("Fetching original content for:", url.pathname);
-    const sourceUrl = new URL(`${domainSource}${url.pathname}`);
+    const sourceUrl = new URL(`${domainSource}${url.pathname}${url.search}`);
     const sourceRequest = new Request(sourceUrl, request);
     const sourceResponse = await fetch(sourceRequest);
 
